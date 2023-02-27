0f09dc311dd357a56a1b469e2b6b6caa073debad
Michelle Steenvoorden <michellesteenvoorden93@gmail.com>
2023-02-27 16:53:52 +0100

    version 1

---
+++ src/main/java/com/example/logging/User.java
@@ -0,0 +1,39 @@
+package com.example.logging;
+
+public class User {
+
+    //private Long id;
+    private String firstName; //final?
+    private String lastName; //final?
+    private Double time;
+
+    public User(String firstName, String lastName, Double time) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.time = time;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Double getTime() {
+        return time;
+    }
+
+    public void setTime(Double time) {
+        this.time = time;
+    }
+}
--- src/main/java/com/example/logging/loggingController.java
+++ src/main/java/com/example/logging/loggingController.java
@@ -1,2 +1,21 @@
-package com.example.logging;public class loggingController {
+package com.example.logging;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class loggingController {
+
+    @GetMapping("/")
+    public String home(){
+        return "logging";
+    }
+
+    @PostMapping("/")
+    public String registration(HttpSession session, User user){
+        session.setAttribute("user", user);
+        return "logging";
+    }
 }
