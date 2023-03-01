c8b87486472274a1ed340f6f5e2adf28deaf4ae2
fregget <fbjuren@gmail.com>
2023-03-01 11:49:47 +0100

    1 mars lunch igen

--- src/main/java/com/example/logging/LoggingController.java
+++ src/main/java/com/example/logging/LoggingController.java
@@ -1,19 +1,14 @@
 package com.example.logging;
-
 import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import java.time.LocalDate;
-import java.util.ArrayList;
 
 @Controller
 public class LoggingController {
